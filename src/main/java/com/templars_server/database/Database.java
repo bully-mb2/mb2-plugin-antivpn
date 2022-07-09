74bdb6fe47f49dd359f6a6c00e616901f48cfcbf
Bully <bully.mb2@gmail.com>
2022-07-09 13:30:53 +0200

    Use connection pooling to prevent connections from closing over time and cleanup a bunch of unused imports

--- src/main/java/com/templars_server/database/Database.java
+++ src/main/java/com/templars_server/database/Database.java
@@ -1,8 +1,10 @@
 package com.templars_server.database;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.templars_server.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.sql.*;
 
 public class Database {
@@ -13,18 +15,22 @@ public class Database {
     private final String table;
     private final String user;
     private final String password;
-    private Connection db;
+    private final ComboPooledDataSource pool;
 
     public Database(String address, String table, String user, String password) {
         this.address = address;
         this.table = table;
         this.user = user;
         this.password = password;
+        this.pool = new ComboPooledDataSource();
+        String uri = String.format("jdbc:mariadb://%s/%s", address, table);
+        pool.setJdbcUrl(uri);
+        pool.setUser(user);
+        pool.setPassword(password);
     }
 
     public void setup() throws SQLException {
-        checkConnection();
-
+        Connection db = pool.getConnection();
         LOG.info("Creating table anti_vpn if it doesn't already exist");
         try (Statement stmt = db.createStatement()) {
             stmt.execute("CREATE TABLE IF NOT EXISTS `anti_vpn` (" +
@@ -38,8 +44,7 @@ public class Database {
     }
 
     public AntiVPNRow getRowByIp(String ip) throws SQLException {
-        checkConnection();
-
+        Connection db = pool.getConnection();
         String sql = "SELECT vpn, vpn_check FROM anti_vpn WHERE ip = ?";
         try (PreparedStatement stmt = db.prepareStatement(sql)) {
             stmt.setString(1, ip);
@@ -53,8 +58,7 @@ public class Database {
     }
 
     public void insertRow(AntiVPNRow antiVPNRow) throws SQLException {
-        checkConnection();
-
+        Connection db = pool.getConnection();
         String sql = "INSERT IGNORE INTO anti_vpn (ip, vpn, vpn_check) VALUES (?, ?, ?)";
         try (PreparedStatement stmt = db.prepareStatement(sql)) {
             stmt.setString(1, antiVPNRow.getIp());
@@ -62,28 +66,6 @@ public class Database {
             stmt.setInt(3, antiVPNRow.isVpnCheck() ? 1 : 0);
             stmt.executeUpdate();
         }
-    }
-
-    private void checkConnection() throws SQLException {
-        if (db == null || db.isClosed()) {
-            connect();
-        }
-    }
-
-    private void connect() throws SQLException {
-        if (db != null) {
-            db.close();
-        }
-
-        String uri = String.format("jdbc:mariadb://%s/%s", address, table);
-        LOG.info("Connecting to database " + uri);
-        db = DriverManager.getConnection(
-                uri,
-                user,
-                password
-        );
-
-        LOG.info("Successfully connected to database");
     }
 
 }
